05421e648602c97cd78f144ba4a68eea6af8ced0
cahnrswebteam <cahnrs.webteam@wsu.edu>
2015-07-16 15:14:25 -0700

    Added Comments

--- cahnrs-lastlogin.php
+++ cahnrs-lastlogin.php
@@ -12,6 +12,15 @@
  * License:     GPLv3
  */
 
+/*
+* Create a page that shows all the users
+* IMPORTANT: check for logged in and that they are an admin!
+* Create a php file called users.php and add the same code used to generate the
+* shortcode results. Again make sure to check for logged in and is admin. 
+* use add_filter | template_include and $_GET params ( getusers & global ).
+* in the filter function check for $_GET[getusers] if true set $template to your users.php file
+* ( you have to pass a fully qualified path ). if global is set to true show all users for the network.
+*/
 
 add_action('wp_login','wpdb_capture_user_last_login', 10, 2);
 function wpdb_capture_user_last_login($user_login){
@@ -61,7 +70,16 @@ $args = array(
 'orderby' => 'meta_value',
 'order'=> 'DESC' );
 
-$blogusers = get_users( $args );
+/* 
+* To Do: add shortcode attribute "global" to get all users
+* in the network. Create an array of the current site's id. If global
+* is set to true use wp get sites to add all site ids to your array.
+* don't forget to do a check so you don't add the current twice ( in_array ).
+* Loop ( foreach ) through all of the ids and set up the value of $args with the site id
+* and then the following foreach loop on the users.
+*/
+
+$blogusers = get_users( $args ); // this is site specific - you can pass site id for multisite
 // Array of stdClass objects.
 $content = "<table class='authors-list'>";
 foreach ( $blogusers as $user ) {
